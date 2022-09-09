a2743f15e085b8cbbd3f0b785633ed39a51beec2
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-09-09 13:08:28 +0300

    feat: add header section added a header section to application along with its styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/Index';
 
 function App() {
   return (
-    <div className="w-screen h-auto flex flex-col ">
+    <div className="w-screen h-auto flex flex-col bg-primary ">
       <Header />
+      <main className="mt-24 p-8 w-full">
+        <Routes>
+          <Route path="/*" element={<MainContainer />} />
+          <Route path="createItem" element={<CreateContainer />} />
+        </Routes>
+      </main>
     </div>
   );
 }
