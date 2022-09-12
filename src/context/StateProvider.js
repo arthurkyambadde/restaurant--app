5a13b2662c6d69427c64dd3ff23bfb1cbf72a53c
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-09-12 13:21:20 +0300

    feat: add dropdown to user profile icon

--- src/context/StateProvider.js
+++ src/context/StateProvider.js
@@ -2,10 +2,10 @@ import React, { createContext, useContext, useReducer } from 'react';
 
 export const stateContext = createContext();
 
-export const StateProvider = ({ reducer, initialState, children }) => {
+export const StateProvider = ({ reducer, initialState, children }) => (
   <stateContext.Provider value={useReducer(reducer, initialState)}>
     {children}
-  </stateContext.Provider>;
-};
+  </stateContext.Provider>
+);
 
 export const useStateValue = () => useContext(stateContext);
