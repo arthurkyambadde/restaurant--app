df553e1312d69837ccfc806cc9b56ef641b1c714
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-09-16 21:16:44 +0300

    fix: fix cart amout

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -6,7 +6,7 @@ import { heroData } from '../utils/data';
 const HomeContainer = () => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full " id="home">
-      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
+      <div className="py-16 flex-1 flex flex-col items-start justify-center gap-6">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">Bike Delivery</p>
           <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
@@ -32,7 +32,7 @@ const HomeContainer = () => {
           Order Now
         </button>
       </div>
-      <div className="py-2 flex-1 flex items-center relative">
+      <div className="py-28 flex-1 flex items-center relative">
         <img src={HeroBg} className=" ml-auto h-420 w-full lg:w-auto lg:h-650" alt="hero-bg" />
 
         <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
